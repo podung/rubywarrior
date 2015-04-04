83dcc5d62a1e7c73a759f32f4086ff6fb101ccad
Joe DePung <joe.depung@gmail.com>
2015-04-03 23:28:14 -0500

    refactor level 3

--- podung-beginner/player.rb
+++ podung-beginner/player.rb
@@ -1,13 +1,34 @@
 class Player
+  attr_accessor :warrior
+
   def play_turn(warrior)
-    if warrior.health < 10
+    self.warrior = warrior
+
+    if sick?
       warrior.rest!
-    elsif warrior.feel.empty? && warrior.health < 20
+    elsif safe? && can_use_more_health?
       warrior.rest!
-    elsif !warrior.feel.empty?
+    elsif danger?
       warrior.attack!
     else
       warrior.walk!
     end
+  end
+
+  private
+  def sick?
+    warrior.health < 10
+  end
+
+  def safe?
+    warrior.feel.empty?
+  end
+
+  def danger?
+    !safe?
+  end
+
+  def can_use_more_health?
+    warrior.health < 20
   end
 end
