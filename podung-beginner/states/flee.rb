859a5820352ebe09e0383e095639c29450a05b51
Joe DePung <joe.depung@gmail.com>
2015-04-13 21:56:51 -0500

    wip - working on level 9 progress

--- podung-beginner/states/flee.rb
+++ podung-beginner/states/flee.rb
@@ -2,10 +2,16 @@ require_relative 'state'
 
 class FleeState < State
   def matches
-    context.near_death? && context.taking_damage? && context.not_in_battle
+    context.near_death? && context.taking_damage? && context.not_in_battle && cannot_see_archer_behind
   end
 
   def respond!
     context.warrior.walk! :backward
+  end
+
+  private
+  def cannot_see_archer_behind
+    first_non_empty_space = context.warrior.look(:backward).find { |space| !space.empty? }
+    first_non_empty_space && !first_non_empty_space.enemy?
   end
 end
