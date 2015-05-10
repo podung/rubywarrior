aeba99992b1e08a2379a781a57a823bad5493645
Joe DePung <joe.depung@gmail.com>
2015-05-09 23:51:35 -0500

    method rename for clarity

--- podung-beginner/states/face_your_enemy.rb
+++ podung-beginner/states/face_your_enemy.rb
@@ -2,7 +2,7 @@ require_relative 'state'
 
 class FaceYourEnemyState < State
   def matches
-    context.taking_damage? && can_see_archer
+    context.taking_damage? && can_see_archer_behind
   end
 
   def respond!
@@ -10,7 +10,7 @@ class FaceYourEnemyState < State
   end
 
   private
-  def can_see_archer
+  def can_see_archer_behind
     non_empty_space = context.warrior.look(:backward).find { |space| !space.empty? }
 
     return non_empty_space &&
