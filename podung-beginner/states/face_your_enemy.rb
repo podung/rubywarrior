ea775e7e1ef5f10c0b08dc5a46fac6326db10a69
Joe DePung <joe.depung@gmail.com>
2015-05-11 23:30:02 -0500

    poor implementation to get further on level

--- podung-beginner/states/face_your_enemy.rb
+++ podung-beginner/states/face_your_enemy.rb
@@ -2,7 +2,8 @@ require_relative 'state'
 
 class FaceYourEnemyState < State
   def matches
-    context.taking_damage? && can_see_archer_behind
+    (context.taking_damage? && can_see_archer_behind) ||
+        enemy_directly_behind?
   end
 
   def respond!
@@ -14,7 +15,13 @@ class FaceYourEnemyState < State
     non_empty_space = context.warrior.look(:backward).find { |space| !space.empty? }
 
     return non_empty_space &&
-      non_empty_space.unit.is_a?(RubyWarrior::Units::Archer)
+            non_empty_space.unit.is_a?(RubyWarrior::Units::Archer)
+  end
+
+  def enemy_directly_behind?
+    space_behind = context.warrior.look(:backward).first
+
+    return space_behind.enemy?
   end
 
 end
